9022d50579a404f4fd73d0e26301f8f84b4d2249
DilipKumarDas <126773381+DilipKumarDas@users.noreply.github.com>
2025-11-22 11:01:37 +0530

    Update api.js

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "task-workflow.up.railway.app:8080"
+    baseURL: "https://taskflow-backend-9m69.onrender.com"
 });
 
 export default api;
